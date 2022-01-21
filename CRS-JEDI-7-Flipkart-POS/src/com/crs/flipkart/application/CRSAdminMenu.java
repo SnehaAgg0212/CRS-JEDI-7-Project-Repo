b612f58e23c6957d7c3db3f0c6908f1a9b6d6cce
SnehaAgg0212 <aggarwalsneha1999@gmail.com>
2022-01-21 22:06:47 +0530

    Add professor and delete professor

--- CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/application/CRSAdminMenu.java
+++ CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/application/CRSAdminMenu.java
@@ -5,6 +5,8 @@ package com.crs.flipkart.application;
 
 import java.util.Scanner;
 
+import com.crs.flipkart.business.AdminService;
+
 /**
  * @author LENOVO
  *
@@ -16,8 +18,37 @@ public class CRSAdminMenu {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		AdminService adminServices = new AdminService();
 		@SuppressWarnings("unused")
 		Scanner sc = new Scanner(System.in);
-	}
 
+		while(true) {
+			System.out.print("Enter the number to perform the task:-\n1. AddProfessor()\n 2. Delete Professor\n3. View Professor\n exit");  
+			int a= sc.nextInt();  
+			
+			switch(a) {
+				case 1: 
+					adminServices.addProfessor();
+					break;
+
+				case 2: 
+					adminServices.deleteProfessor();
+					break;
+				
+				case 3: 
+					adminServices.viewProfessor();
+					break;
+
+				default:
+					break;
+			}
+			
+			if(a > 3) {
+				break;
+			}
+		}
+		sc.close();
+	}
+	
 }
--- CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/business/AdminService.java
+++ CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/business/AdminService.java
@@ -2,11 +2,78 @@
  * 
  */
 package com.crs.flipkart.business;
-
+import java.util.*;
+import com.crs.flipkart.bean.Professor;
 /**
  * @author LENOVO
  *
  */
 public class AdminService {
+    Scanner sc = new Scanner(System.in);
+    static int profId = 100;
+    static int userId = 1000;
 
+    private void profIdincrementor(){
+        profId ++;
+    }
+    private void userIdincrementor(){
+        userId ++;
+    }
+    Vector<Professor> ProfessorList = new Vector<Professor>();
+
+ public void addProfessor() {
+    String role = "Professor";
+    int professorId = profId;
+    System.out.println("Enter details of the Professor to be added: ");
+    System.out.print("Enter the name of the professor: ");
+    String professorName = sc.nextLine();
+    System.out.print("Enter the professor’s Email Id: ");
+    String emailId = sc.nextLine();
+    System.out.print("Enter Password - ");
+    String password = sc.nextLine();
+    System.out.print("Enter contact number - ");
+    String phoneNo = sc.nextLine();
+    System.out.print("Enter Department designated - ");
+    String department = sc.nextLine();
+    System.out.print("Enter designation - ");
+    String designation = sc.nextLine();
+    System.out.print("Enter gender: ");
+    String gender = sc.nextLine();
+    System.out.print("Enter address: ");
+    String address = sc.nextLine();
+    profIdincrementor();
+    userIdincrementor();
+ 
+    Professor prof = new Professor(professorName, emailId, password, role, userId, phoneNo,
+    gender, address, professorId, department, designation);
+
+    ProfessorList.add(prof);
+}
+
+public void viewProfessor(){
+    System.out.println("Enter the professor ID to view: " );
+    int professorId = sc.nextInt();
+
+    for(int i=0; i< ProfessorList.size(); i++){
+        if(ProfessorList.get(i)!= null && professorId == ProfessorList.get(i).getProfessorId()){
+            System.out.println(ProfessorList.get(i).getProfessorId());
+            System.out.println(ProfessorList.get(i).getDepartment());
+            System.out.println(ProfessorList.get(i).getDesignation());
+            break;
+        }  
+    }
+}
+
+ public void deleteProfessor(){
+    System.out.println("Enter the professor ID for deletion" );
+    int professorId = sc.nextInt();
+
+    for(int i=0; i< ProfessorList.size(); i++){
+        if(ProfessorList.get(i)!= null && professorId == ProfessorList.get(i).getProfessorId()){
+            ProfessorList.removeElementAt(i);
+            System.out.println("Professor is deleted");
+            break;
+        }
+    }
+ }
 }
