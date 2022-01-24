a3e08c64d58ea99a7f5cc4d8a64a633c7d4c949f
Devanshu Garg <65565864+codedreamer-dg@users.noreply.github.com>
2022-01-24 11:28:06 +0530

    Update AdminService.java

--- CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/business/AdminService.java
+++ CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/business/AdminService.java
@@ -11,27 +11,27 @@ import com.crs.flipkart.bean.GradeCard;
 public class AdminService {
 	
 	GradeCard grades[]=new GradeCard[] {
-			new GradeCard(414,"Software Engineering",1,9.8,7),
-			new GradeCard(213,"Modern Algebra",1,8,7),
-			new GradeCard(212,"Software Project Management",1,10,7)
+			new GradeCard(414, "Software Engineering", 1, 9.8, 7),
+			new GradeCard(213, "Modern Algebra", 1, 8, 7),
+			new GradeCard(212, "Software Project Management", 1, 10, 7)
 	};
 	
-	public void GenerateGradeCard(int studentId,int semesterId)
+	public void GenerateGradeCard(int studentId, String studentName, int semesterId)
 	{
-		System.out.println("Semester:"+semesterId);
+		System.out.println("Semester: " + semesterId);
 		
-		System.out.println("StudentName: "+" StudentId:"+studentId);
+		System.out.println(" StudentId: " + studentId + " StudentName: " + studentName);
 		
-		double overallGpa=0.0;
+		double overallGpa = 0.0;
 
 		for(GradeCard course_grade:grades) {
-			System.out.println(course_grade.getCourseId()+" "+course_grade.getCourseName()+" "+course_grade.getGpa());
-			overallGpa+=course_grade.getGpa();
+			System.out.println(course_grade.getCourseId() + " " + course_grade.getCourseName() + " " + course_grade.getGpa());
+			overallGpa += course_grade.getGpa();
 		}
 		
-		overallGpa/=(double)grades.length;
+		overallGpa /= (double)grades.length;
 		
-		System.out.println("Overall GPA:"+overallGpa);
+		System.out.println("Overall GPA: " + overallGpa);
 		
 	}
 	
