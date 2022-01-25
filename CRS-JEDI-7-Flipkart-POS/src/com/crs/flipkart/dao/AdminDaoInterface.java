8858309a41650173d90b617fbd11d6c2ac6ccfcc
Devanshu Garg <gargdevanshu0804cse@gmail.com>
2022-01-25 12:24:05 +0530

    Modified AdminDaoOperation

---
+++ CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/dao/AdminDaoInterface.java
@@ -0,0 +1,68 @@
+package com.crs.flipkart.dao;
+
+import java.util.Vector;
+
+import com.crs.flipkart.bean.Course;
+import com.crs.flipkart.bean.Professor;
+import com.crs.flipkart.bean.RegisteredCourse;
+import com.crs.flipkart.bean.User;
+
+public interface AdminDaoInterface {
+
+	/**
+	 * 
+	 * @param professor
+	 */
+	void addProfessor(Professor professor);
+
+	/**
+	 * 
+	 * @return
+	 */
+	Vector<Professor> viewProfessor();
+
+	/**
+	 * 
+	 * @param professorId
+	 */
+	void deleteProfessor(int professorId);
+
+	/**
+	 * 
+	 * @param studentId
+	 */
+	void approveStudentRegistration(int studentId);
+
+	/**
+	 * 
+	 * @param studentId
+	 * @param semesterId
+	 * @return
+	 */
+	Vector<RegisteredCourse> generateGradeCard(int studentId, int semesterId);
+
+	/**
+	 * 
+	 * @param course
+	 */
+	void addCourse(Course course);
+
+	/**
+	 * 
+	 * @return
+	 */
+	Vector<Course> viewCourse();
+
+	/**
+	 * 
+	 * @param courseId
+	 */
+	void deleteCourse(int courseId);
+
+	/**
+	 * 
+	 * @param user
+	 */
+	void addUser(User user);
+
+}
--- CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/dao/AdminDaoOperation.java
+++ CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/dao/AdminDaoOperation.java
@@ -8,10 +8,55 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import com.crs.flipkart.bean.Course;
+import com.crs.flipkart.bean.Professor;
+import com.crs.flipkart.bean.RegisteredCourse;
+import com.crs.flipkart.bean.User;
+
+import java.util.Vector;
+
 /**
  * @author devanshugarg
  *
  */
 public class AdminDaoOperation {
 
+	public void addProfessor(Professor professor) {
+		
+	}
+	
+	public Vector<Professor> viewProfessor() {
+		
+		return null;
+	}
+	
+	public void deleteProfessor(int professorId) {
+		
+	}
+	
+	public void approveStudentRegistration(int studentId) {
+		
+	}
+	
+	public Vector<RegisteredCourse> generateGradeCard(int studentId, int semesterId) {
+		
+		return null;
+	}
+	
+	public void addCourse(Course course) {
+		
+	}
+	
+	public Vector<Course> viewCourse() {
+		
+		return null;
+	}
+	
+	public void deleteCourse(int courseId) {
+		
+	}
+	
+	public void addUser(User user) {
+		
+	}
 }
