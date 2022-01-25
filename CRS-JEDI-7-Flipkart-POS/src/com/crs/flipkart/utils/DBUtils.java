78f113e6ea91a6757a40c73b9033c63b73db3fb7
Devanshu Garg <gargdevanshu0804cse@gmail.com>
2022-01-25 14:43:53 +0530

    Modifications in DBUtils

--- CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/utils/DBUtils.java
+++ CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/utils/DBUtils.java
@@ -18,6 +18,32 @@ public class DBUtils {
 	
 	public static Connection getConnection() {
 		
-		return connection;
+		if (connection != null) {
+			try {
+				if (connection.isClosed()) {	
+					connection = null;
+					return getConnection();
+				} else {
+				return connection;
+				}
+			} catch (SQLException e) {
+				return getConnection();
+			}
+		} else {
+			try {
+				Properties properties = new Properties();
+				String driver = "com.mysql.cj.jdbc.Driver";
+				String url = "jdbc:mysql://localhost:3306/test";
+				String username = "root";
+				String password = "root";
+				Class.forName(driver);
+				connection = DriverManager.getConnection(url, username, password);
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			return connection;
+		}
 	}
 }
