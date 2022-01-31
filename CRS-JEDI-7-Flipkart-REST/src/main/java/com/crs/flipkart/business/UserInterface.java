0f08be58d36ef1b720bc2596d2451d68f593badd
Devanshu Garg <gargdevanshu0804cse@gmail.com>
2022-01-31 15:51:37 +0530

    REST Structure

---
+++ CRS-JEDI-7-Flipkart-REST/src/main/java/com/crs/flipkart/business/UserInterface.java
@@ -0,0 +1,38 @@
+package com.crs.flipkart.business;
+
+import com.crs.flipkart.exceptions.UserNotFoundException;
+
+public interface UserInterface {
+
+	/**
+	 * @param userId
+	 * @return
+	 */
+	String getRoleOfUser(String userEmailId);
+
+	/**
+	 * @param emailId
+	 * @param password
+	 * @return
+	 * @throws UserNotFoundException 
+	 */
+	boolean validateUser(String emailId, String password) throws UserNotFoundException;
+
+	/**
+	 * 
+	 * @param userEmailId
+	 * @param oldPassword
+	 * @param newPassword
+	 * @param confirmNewPassword
+	 * @throws UserNotFoundException 
+	 */
+	void updatePassword(String userEmailId, String oldPassword, String newPassword, String confirmNewPassword) throws UserNotFoundException;
+
+	/**
+	 * 
+	 * @param userEmailId
+	 * @return
+	 */
+	int getUserId(String userEmailId);
+
+}
---
+++ CRS-JEDI-7-Flipkart-REST/src/main/java/com/crs/flipkart/restController/StudentRestAPI.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.crs.flipkart.restController;
+
+/**
+ * @author devanshugarg
+ *
+ */
+public class StudentRestAPI {
+
+}
